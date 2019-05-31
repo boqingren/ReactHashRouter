3d55399a4bdc261fd1bb066b85880cf28909e2bb
boqingren <boqingren@hotmail.com>
2019-05-31 15:33:05 +0800

    随便写写用户详情页面

--- src/Pages/UserDetails.js
+++ src/Pages/UserDetails.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useContext } from "react";
+import ReduxContext from "../Store/ReduxContext";
 
 export default props => {
+    const { state } = useContext(ReduxContext);
+    const { userList } = state;
+    const { params } = props.match;
+    const list = Array.isArray(userList)? userList: [];
+    const [ uesr ] = list.filter(item => parseInt(params.id) === item.id);
+
     return (
         <div>
-            UserDetails
+            <div>id: {uesr.id}</div>
+            <div>username: {uesr.username}</div>
+            <div>nickname: {uesr.nickname}</div>
+            <div>password: {uesr.password}</div>
         </div>
     );
 };
